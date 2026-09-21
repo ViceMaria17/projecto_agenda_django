ce59d7a92d2702604ee7a400bf43180efbb642b9
ViceMaria17 <vicente.maria.vm17@gmail.com>
2026-09-21 14:47:04 +0200

    Created a script that automaticaly fills the database using faker

--- contact/models.py
+++ contact/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -20,7 +22,16 @@ class Contact(models.Model):
     description = models.TextField(max_length=200, blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
-    catergory = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    catergory = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL, 
+        blank=True, null=True
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
